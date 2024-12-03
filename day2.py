3541ae3035e33ef1ff42312c17131c8494387be5
saurabh10041998 <shindesaurabhpict@gmail.com>
2024-12-03 11:33:17 +0530

    day2 part2, not completed yet

--- day2.py
+++ day2.py
@@ -1,4 +1,6 @@
 import sys
+from typing import Tuple
+from typing import Type
 from typing import Sequence
 from enum import Enum
 
@@ -32,6 +34,14 @@ class Levels:
         else:
             return LevelType.FLAT
 
+    @classmethod
+    def skip_lvl(
+        cls: Type['Levels'],
+        seq: Sequence[int],
+        lvl: int
+    ) -> 'Levels':
+        return cls(tuple(seq[:lvl]) + tuple(seq[lvl + 1:]))
+
     @staticmethod
     def _safe_diff(x: int, y: int) -> bool:
         return 1 <= (x - y) <= 3
@@ -58,6 +68,41 @@ class Levels:
                 ]
             )
 
+    def unsafe_at(self) -> Tuple[int, int]:
+        assert not self.is_safe()
+
+        typ = self._guess_type()
+
+        if typ == LevelType.FLAT:
+            return (0, 1)
+
+        elif typ == LevelType.INCREASING:
+            curr_idx = 1
+            while curr_idx < len(self.seq):
+                prev_idx = curr_idx - 1
+
+                if not self._safe_diff(
+                    self.seq[curr_idx],
+                    self.seq[prev_idx]
+                ):
+                    return (prev_idx, curr_idx)
+
+                curr_idx += 1
+
+        else:  # LevelType.DECREASING
+            curr_idx = 1
+            while curr_idx < len(self.seq):
+                prev_idx = curr_idx - 1
+
+                if not self._safe_diff(
+                    self.seq[prev_idx],
+                    self.seq[curr_idx]
+                ):
+                    return (prev_idx, curr_idx)
+
+                curr_idx += 1
+        raise AssertionError("Should not reach here for unsafe sequence")
+
 
 def part1(input_s: str) -> int:
     ans = 0
@@ -66,4 +111,21 @@ def part1(input_s: str) -> int:
             s = Levels(tuple(int(x) for x in line.split()))
             if s.is_safe():
                 ans += 1
+    return ans
+
+
+def part2(input_s: str) -> int:
+    ans = 0
+    for line in input_s.split("\n"):
+        if line:
+            s = Levels(tuple(int(x) for x in line.split()))
+
+            if s.is_safe():
+                ans += 1
+            else:
+                lv1, lv2 = s.unsafe_at()
+                s1 = s.skip_lvl(s.seq, lv1)
+                s2 = s.skip_lvl(s.seq, lv2)
+                if s1.is_safe() or s2.is_safe():
+                    ans += 1
     return ans
--- tests/day2_test.py
+++ tests/day2_test.py
@@ -1,6 +1,7 @@
 import pytest
 from pathlib import Path
 from day2 import part1
+from day2 import part2
 from day2 import Levels
 from day2 import LevelType
 
@@ -64,3 +65,56 @@ def test_part1():
     with open(input_f, 'r') as f:
         input_s = f.read()
     assert part1(input_s) == 663
+
+
+@pytest.mark.parametrize(
+    ("input_l", "expected"),
+    (
+        ((1, 2, 7, 8, 9), (1, 2)),
+        ((9, 7, 6, 2, 1), (2, 3)),
+        ((1, 3, 2, 4, 5), (1, 2)),
+        ((8, 6, 4, 4, 1), (2, 3)),
+    )
+)
+def test_unsafe_at(input_l, expected):
+    s = Levels(input_l)
+    assert s.unsafe_at() == expected
+
+
+@pytest.mark.parametrize(
+    ("input_l", "lvl", "expected"),
+    (
+        ((1, 2, 7, 8, 9), 1, (1, 7, 8, 9)),
+        ((1, 2, 7, 8, 9), 2, (1, 2, 8, 9)),
+        ((9, 7, 6, 2, 1), 2, (9, 7, 2, 1)),
+        ((9, 7, 6, 2, 1), 3, (9, 7, 6, 1)),
+        ((1, 3, 2, 4, 5), 1, (1, 2, 4, 5)),
+        ((1, 3, 2, 4, 5), 2, (1, 3, 4, 5)),
+        ((8, 6, 4, 4, 1), 2, (8, 6, 4, 1)),
+        ((8, 6, 4, 4, 1), 3, (8, 6, 4, 1)),
+    )
+)
+def test_skip_lvl(input_l, lvl, expected):
+    s = Levels(input_l)
+    new_s = s.skip_lvl(s.seq, lvl)
+    new_s.seq == expected
+
+
+def test_part2_min():
+    input_s = """\
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+"""
+    assert part2(input_s) == 4
+
+
+@pytest.mark.xfail(reason="Bug in the part2 implmentation")
+def test_part2():
+    input_f = Path(__file__).parent / "data" / "day2.txt"
+    with open(input_f, 'r') as f:
+        input_s = f.read()
+    assert part2(input_s) == 663
